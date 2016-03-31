1c973d8159c1d548fc6df2f887aa8a1870b89dee
Nathaniel Higgins <nat@nath.is>
2016-03-31 20:45:44 +0100

    Add autoplay and click to toggle

--- src/renderer/components/player.jsx
+++ src/renderer/components/player.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
 export class VideoPlayer extends React.Component {
+
+	onClick() {
+		const video = this.refs.video.getDOMNode();
+
+		if (!video.paused) {
+			video.pause();
+		} else {
+			video.play();
+		}
+
+	}
+
     render() {
-        return <video poster={this.props.poster} src={this.props.src} controls></video>
+        return <video
+        	autoPlay
+        	ref="video"
+        	poster={this.props.poster}
+        	src={this.props.src}
+        	controls
+        	onClick={::this.onClick}
+        ></video>
     }
 }
